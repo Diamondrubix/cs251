31a2c0bc05d2d6dc047c9f060931f4aef8a103c3
diamondrubix <diamondrubix@gmail.com>
2018-01-19 11:16:38 -0600

    first test

---
+++ firsttest/cmake-build-debug/CMakeFiles/firsttest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/firsttest.dir/main.cpp.o"
+  "firsttest.pdb"
+  "firsttest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/firsttest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
