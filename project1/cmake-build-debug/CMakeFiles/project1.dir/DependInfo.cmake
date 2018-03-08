78383386380d7d68a40e1bbefc9794088b66c3b8
aarato <diamondrubix@gmail.com>
2018-03-08 17:31:42 -0600

    pre move transfer

--- project1/cmake-build-debug/CMakeFiles/project1.dir/DependInfo.cmake
+++ project1/cmake-build-debug/CMakeFiles/project1.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/diamondrubix/CLionProjects/cs251/project1/main.cpp" "/home/diamondrubix/CLionProjects/cs251/project1/cmake-build-debug/CMakeFiles/project1.dir/main.cpp.o"
+  "/home/aarato/CLionProjects/cs251/project1/main.cpp" "/home/aarato/CLionProjects/cs251/project1/cmake-build-debug/CMakeFiles/project1.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
